41194600259ce23468665be4b838ff513196ad05
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2023-08-23 14:15:28 +0300

    feat: Response read using io

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -12,5 +13,10 @@ func main() {
 		fmt.Println("Error", err)
 		os.Exit(1)
 	}
-	fmt.Println(resp)
+
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	io.Copy(os.Stdout, resp.Body)
 }
