c7dee5e232aa503cef63c5aea213f3a25048779d
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2023-08-23 15:34:25 +0300

    feat: first few lines of code added

--- main.go
+++ main.go
@@ -2,33 +2,6 @@ package main
 
 import "fmt"
 
-type square struct {
-	sideLength float64
-}
-
-type triangle struct {
-	height float64
-	base   float64
-}
-
-type shape interface {
-	getArea() float64
-}
-
-func (t triangle) getArea() float64 {
-	return 0.5 * t.base * t.height
-}
-func (s square) getArea() float64 {
-	return s.sideLength * s.sideLength
-}
-func printArea(s shape) {
-	fmt.Println("The area is as such:", s.getArea())
-}
-
 func main() {
-	fmt.Println("Hello World, Here is how this works")
-	square := square{sideLength: 2}
-	triangle := triangle{base: 2, height: 2}
-	printArea(square)
-	printArea(triangle)
+	fmt.Println("Hello World!")
 }
