6e09d3b14d8ad8c7c29108086dddecaf9d430ccc
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2023-08-23 14:49:23 +0300

    feat: New branch + new file generated

--- main.go
+++ main.go
@@ -1,30 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-)
-
-type newWriter struct{}
+import "fmt"
 
 func main() {
-	resp, err := http.Get("http://google.com")
-	if err != nil {
-		fmt.Println("Error", err)
-		os.Exit(1)
-	}
-
-	// bs := make([]byte, 99999)
-	// resp.Body.Read(bs)
-	// fmt.Println(string(bs))
-
-	io.Copy(os.Stdout, resp.Body)
-}
-
-func (newWriter) Write(bs []byte) (int, error) {
-	fmt.Println(string(bs))
-	fmt.Println("This was a custom writer function that I made!n I wonder what are some of the other functions that could work this way!")
-	return len(bs), nil
+	fmt.Println("Hello World!")
 }
